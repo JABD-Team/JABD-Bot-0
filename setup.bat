df2926b08ab37ccdae4e330ca6d741aec6f793e8
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2021-09-10 18:03:53 +0700

    Idk 🙈

--- setup.bat
+++ setup.bat
@@ -1,1 +1,5 @@
-npm i
+@echo off
+echo "When you agree to proceed with the installation, node_modules will take up a certain amount of space."
+pause
+"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" npm i ; npm i electron
+pause
