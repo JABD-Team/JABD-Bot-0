d7ced31a28e6e6af2327a129e65feb3a4f46469e
Đặng Phong <thanhphongdang14@gmail.com>
2021-11-01 17:34:35 +0700

    Delete setup.bat

--- setup.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo "When you agree to proceed with the installation, node_modules will take up a certain amount of space."
-pause
-"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" npm i ; npm i electron
-pause
