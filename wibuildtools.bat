09be6233f2b2c5fc74d592ba9931f3cde9f04b14
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2021-11-01 18:40:01 +0700

    Update wibuildtools.bat

--- wibuildtools.bat
+++ wibuildtools.bat
@@ -1,3 +1,9 @@
 @echo off
+echo -When you press "Enter" the system will start upgrading Python and Visualstudio2017 for you.
+echo *Note: In addition to installing Python and Visualstudio2017, the system also installs Chocolatey to support the installation!
+echo ===============================================
+echo Tool created by HerokeyVN.
+pause
+cls
 "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')); choco upgrade -y python visualstudio2017-workload-vctools
 pause
