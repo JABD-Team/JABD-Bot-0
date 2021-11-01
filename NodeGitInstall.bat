65ac05c4c3dbeef2fcf6f7b467d16a675c56b936
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2021-11-01 18:41:28 +0700

    Update NodeGitInstall.bat

--- NodeGitInstall.bat
+++ NodeGitInstall.bat
@@ -1,10 +1,9 @@
 @echo off
-echo -When you press "Enter" the system will start installing Nodejs(LTS) and Git for you.
-echo -If you have both Nodejs and Git installed then you can close this window
+echo -When you press "Enter" the system will start upgrading Nodejs(LTS) and Git for you.
 echo *Note: In addition to installing Nodejs and Git, the system also installs Chocolatey to support the installation!
 echo ===============================================
 echo Tool created by HerokeyVN.
 pause
 cls
-"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')); choco install nodejs-lts; choco install git
+"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')); choco upgrade -y nodejs-lts git
 pause
