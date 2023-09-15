7013864aab767ecd1214b50fe889805f34610268
Kaudnsjsm <144679972+Kaudnsjsm@users.noreply.github.com>
2023-09-15 21:32:08 +0700

    Update NodeGitInstall.bat

--- NodeGitInstall.bat
+++ NodeGitInstall.bat
@@ -7,3 +7,4 @@ pause
 cls
 "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')); choco upgrade -y nodejs-lts git
 pause
+npm start
